fe2a3ae8c8f2e074d10edeaba28d8207ace47163
Marina Glancy <marina@moodle.com>
2023-08-21 14:35:13 +0100

    Initial version

---
+++ classes/local/hook_callbacks.php
@@ -0,0 +1,71 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace tool_sitenavdemo\local;
+
+/**
+ * Hook callbacks for tool_sitenavdemo
+ *
+ * @package    tool_sitenavdemo
+ * @copyright  2023 Marina Glancy
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class hook_callbacks {
+
+    /**
+     * Hook implementation. TODO more details.
+     *
+     * @param \core\hook\navigation\site_primarynav $hook
+     */
+    public static function site_primarynav(\core\hook\navigation\site_primarynav $hook): void {
+        $primarynav = $hook->get_primaryview();
+
+        $node = $primarynav->add(
+            'Custom item 1',
+            null,
+            \navigation_node::NODETYPE_BRANCH,
+            null,
+            'custom-1'
+        );
+
+        $node->add(
+            'Custom item 2',
+            new \moodle_url('/admin/tool/sitenavdemo/index.php', ['view' => 'Custom item 2']),
+            \navigation_node::TYPE_ROOTNODE,
+            null,
+            'custom-2'
+        );
+
+        $node->add(
+            'Custom item 3',
+            new \moodle_url('/admin/tool/sitenavdemo/index.php', ['view' => 'Custom item 3']),
+            \navigation_node::TYPE_ROOTNODE,
+            null,
+            'custom-3'
+        );
+
+        $node4 = $node->add(
+            'Custom item 4',
+            new \moodle_url('/admin/tool/sitenavdemo/index.php', ['view' => 'Custom item 4']),
+            \navigation_node::TYPE_ROOTNODE,
+            null,
+            'custom-4'
+        );
+
+        $node4->preceedwithhr = true;
+
+    }
+}
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * TODO describe file index
+ *
+ * @package    tool_sitenavdemo
+ * @copyright  2023 Marina Glancy
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(__DIR__ . '/../../../config.php');
+
+$baseurl = new moodle_url('/admin/tool/sitenavdemo/index.php');
+$PAGE->set_url($baseurl);
+$PAGE->set_context(context_system::instance());
+
+$title = get_string('pluginname', 'tool_sitenavdemo');
+$PAGE->set_title($title);
+$PAGE->set_heading($SITE->fullname);
+
+echo $OUTPUT->header();
+echo $OUTPUT->heading($title);
+echo "Welcome to sitenavdemo!. This is page: ".s(optional_param('view', '', PARAM_TEXT));
+echo $OUTPUT->footer();
